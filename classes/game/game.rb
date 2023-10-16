1e60a8c34cd14690b55f5cb985df086a3193f9b7
Rysth <johnpalacios.t@gmail.com>
2023-10-16 11:48:45 -0500

    Create private method can_be_archive? in Game class.

--- classes/game/game.rb
+++ classes/game/game.rb
@@ -8,4 +8,14 @@ class Game < Item
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
+
+  private
+
+  def can_be_archived?()
+    tab_date = @last_played_at.split('/')
+    year = 2023 - tab_date[0].to_i
+    return true if super(can_be_archived) && (year > 2)
+
+    false
+  end
 end
