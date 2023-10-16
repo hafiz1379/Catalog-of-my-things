3df41a79b8dee3fcde228f545910de84634ba21d
Rysth <johnpalacios.t@gmail.com>
2023-10-16 11:47:58 -0500

    Created Game class who inherits from Item with multiplayer and last_player_at properties.

--- classes/game/game.rb
+++ classes/game/game.rb
@@ -1,4 +1,4 @@
-require 'item'
+require_relative '../item'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
@@ -7,15 +7,5 @@ class Game < Item
     super()
     @multiplayer = multiplayer
     @last_played_at = last_played_at
-  end
-
-  private
-
-  def can_be_archived?()
-    tab_date = @last_played_at.split('/')
-    year = 2023 - tab_date[0].to_i
-    return true if super(can_be_archived) && (year > 2)
-
-    false
   end
 end
