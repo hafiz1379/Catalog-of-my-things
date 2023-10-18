cc7010637e448b697643cefaf7ef75281155e924
Juan Diego Guerra <jdxelric@gmail.com>
2023-10-18 13:14:58 -0500

    Update author_module.rb
    Co-Authored-By: John Palacios <115722896+Rysth@users.noreply.github.com>
    Co-Authored-By: Hafiz Rasa <117447018+hafiz1379@users.noreply.github.com>

--- modules/author_module.rb
+++ modules/author_module.rb
@@ -4,12 +4,12 @@ module AuthorModule
       puts 'Please, create an AUTHOR. [Press ENTER to continue]'
       gets.chomp
     else
-      puts 'Authors'
-      puts '-----------------------------'
+      puts 'Authors'.bold.black.on_light_cyan
+      puts '-----------------------------'.black.on_light_cyan
       collection.each do |author|
         puts "ID: #{author.id}, Name: #{author.first_name} #{author.last_name}, Items(Quantity): #{author.items.size}"
       end
-      puts '-----------------------------'
+      puts '-----------------------------'.black.on_light_cyan
     end
   end
 end
